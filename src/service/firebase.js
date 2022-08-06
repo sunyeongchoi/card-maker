4432cb1b7cdb6f359443ce4176154d3ba78303ff
sn0716 <sn0716@naver.com>
2022-08-06 18:20:02 +0900

    project set up & firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig)
