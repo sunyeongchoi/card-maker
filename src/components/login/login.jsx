ce100b912044e12488e0e58d00af73527f9b600d
sn0716 <sn0716@naver.com>
2022-08-11 00:13:29 +0900

    logout, maker 구현

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,12 +2,24 @@ import * as React from 'react';
 import styles from './login.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({authService}) => {
+  const navigate = useNavigate();
+  const goToMaker = (userId) => {
+    navigate('/maker', { state: {id: userId}})
+  }
   const handleLogin = (e) => {
     authService.login(e.currentTarget.textContent)
-    .then(console.log);
+    .then(data => goToMaker(data.user.uid));
   }
+
+  React.useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    })
+  }, []);
+
   return (
     <section className={styles.login}>
       <Header />
