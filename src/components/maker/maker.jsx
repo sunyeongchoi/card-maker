72d1a4d79b8e1d2b6ac9bdf6716c79f158f03182
sn0716 <sn0716@naver.com>
2022-08-22 21:25:13 +0900

    editor and preview UI

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = React.useState([
+        {
+            id: '1',
+            name: 'snchoi',
+            company: 'Google',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'snchoi@gmail.com',
+            message: 'go for it',
+            fileName: 'snchoi',
+            fileURL: null
+        },
+        {
+            id: '2',
+            name: 'snchoi2',
+            company: 'Google',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'snchoi@gmail.com',
+            message: 'go for it',
+            fileName: 'snchoi',
+            fileURL: null
+        },
+        {
+            id: '3',
+            name: 'snchoi3',
+            company: 'Google',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'snchoi@gmail.com',
+            message: 'go for it',
+            fileName: 'snchoi',
+            fileURL: null
+        }
+    ])
     const navigate = useNavigate();
     const onLogout = () => {
         authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
